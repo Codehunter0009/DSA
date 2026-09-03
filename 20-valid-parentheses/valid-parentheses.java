708b282aedb076d36307d8a37860d235551afb31
koushik.N <141755807+Codehunter0009@users.noreply.github.com>
2026-09-03 15:44:37 +0530

    Time: 5 ms (13.16%) | Memory: 43.5 MB (25.64%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,26 +1,42 @@
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> stack=new Stack<>();
-        
+        HashMap<Character,Character> map=new HashMap<>();
+        map.put('(',')');
+        map.put('[',']');
+        map.put('{','}');
         for(int i=0;i<s.length();i++){
-            if(s.charAt(i)=='('||s.charAt(i)=='{'||s.charAt(i)=='['){
-                stack.push(s.charAt(i));
-            }
+            
+            
+               if(map.containsKey(s.charAt(i))){stack.push(map.get(s.charAt(i)));}
             else{
-                if(stack.isEmpty())return false;
-                char ch=stack.peek();
-                if(s.charAt(i)==')'&& ch=='('|| 
-                   s.charAt(i)=='}'&& ch=='{'||
-                   s.charAt(i)==']'&&ch=='['){
+                 if(stack.isEmpty() || stack.peek() != s.charAt(i)) {
+                    return false;
+                }
                 stack.pop();
-
             }
-            else{
-                return false;
-            }
-            }
-
+           
         }
-        return stack.isEmpty();
+         return stack.isEmpty();
+        // for(int i=0;i<s.length();i++){
+        //     if(s.charAt(i)=='('||s.charAt(i)=='{'||s.charAt(i)=='['){
+        //         stack.push(s.charAt(i));
+        //     }
+        //     else{
+        //         if(stack.isEmpty())return false;
+        //         char ch=stack.peek();
+        //         if(s.charAt(i)==')'&& ch=='('|| 
+        //            s.charAt(i)=='}'&& ch=='{'||
+        //            s.charAt(i)==']'&&ch=='['){
+        //         stack.pop();
+
+        //     }
+        //     else{
+        //         return false;
+        //     }
+        //     }
+
+        // }
+        // return stack.isEmpty();
     }
 }
